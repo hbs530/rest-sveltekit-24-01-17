cce956a9c5fe9fc38438be167c925b4edcb04a10
hbs <hbs530@naver.com>
2024-02-11 15:27:09 +0900

    26강, 글 수정기능 구현

--- src/main/java/com/ll/rsv/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rsv/domain/post/post/controller/ApiV1PostController.java
@@ -1,22 +1,17 @@
 package com.ll.rsv.domain.post.post.controller;
 
-import java.util.List;
-
-import org.springframework.lang.NonNull;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ll.rsv.domain.post.post.dto.PostDto;
 import com.ll.rsv.domain.post.post.entity.Post;
 import com.ll.rsv.domain.post.post.service.PostService;
 import com.ll.rsv.global.exceptions.GlobalException;
 import com.ll.rsv.global.rsData.RsData;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -24,16 +19,8 @@ import lombok.RequiredArgsConstructor;
 public class ApiV1PostController {
 	private final PostService postService;
 
-	@Getter
-	public static class GetPostsResponseBody {
-		@NonNull
-		private List<PostDto> items;
 
-		public GetPostsResponseBody(List<Post> items) {
-			this.items = items.stream()
-					.map(PostDto::new)
-					.toList();
-		}
+	public record GetPostsResponseBody(@NonNull List<PostDto> items) {
 	}
 
 	@GetMapping("")
@@ -41,17 +28,16 @@ public class ApiV1PostController {
 		List<Post> items = postService.findByPublished(true);
 
 		return RsData.of(
-				"200-1",
-				"성공",
-				new GetPostsResponseBody(items)
+				new GetPostsResponseBody(
+						items.stream()
+								.map(PostDto::new)
+								.toList()
+				)
 		);
 	}
 
-	@AllArgsConstructor
-	@Getter
-	public static class GetPostResponseBody {
-		@NonNull
-		private PostDto item;
+
+	public record GetPostResponseBody(@NonNull PostDto item) {
 	}
 
 	@GetMapping("/{id}")
@@ -61,9 +47,28 @@ public class ApiV1PostController {
 		Post post = postService.findById(id).orElseThrow(GlobalException.E404::new);
 
 		return RsData.of(
-				"200-1",
-				"성공",
 				new GetPostResponseBody(new PostDto(post))
+		);
+	}
+
+
+	public record EditRequestBody(@NotBlank String title, @NotBlank String body) {
+	}
+
+	public record EditResponseBody(@NonNull PostDto item) {
+	}
+
+	@PutMapping(value = "/{id}")
+	public RsData<EditResponseBody> edit(
+			@PathVariable long id,
+			@Valid @RequestBody EditRequestBody requestBody
+	) {
+		Post post = postService.findById(id).orElseThrow(GlobalException.E404::new);
+
+		postService.edit(post, requestBody.title, requestBody.body);
+
+		return RsData.of(
+				new EditResponseBody(new PostDto(post))
 		);
 	}
 }
